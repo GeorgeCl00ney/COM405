cfa1ed6c1612bfcc2aba8ff2a1e0fda6cfd0902b
Richard Haines <4hainr84@solent.ac.uk>
2019-10-29 16:07:40 +0000

    Added cars array, CarPark constructor, addCar and removeCar methods to CarPark class

--- Week6/src/week6/Car.java
+++ Week6/src/week6/Car.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package week6;
 
-/**
- *
- * @author 4hainr84
- */
 public class Car {
     //ATTRIBUTES
     private String registration;
@@ -28,5 +20,4 @@ public class Car {
     {
         return colour;
     }
-    
 }
--- Week6/src/week6/CarPark.java
+++ Week6/src/week6/CarPark.java
@@ -1,14 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package week6;
 
-/**
- *
- * @author 4hainr84
- */
 public class CarPark {
-    
+//Start with an array attribute to hold Cars
+    private Car[] cars;
+//Create a constructor which instantiates he array with a limit of 15 cars
+    public CarPark()
+    {
+        cars = new Car[15];
+    }
+//Create a method to allow cars to be added to the CarPark object
+    public boolean addCar(Car carIn)
+    {
+        //Need a loop to find an empty element in the array to put the car into
+        for(int i=0; i< cars.length; i+=1)
+        {
+            //if the space is empty
+            if(cars[i] == null)
+            {
+                //put the car into it
+                cars[i] = carIn;
+                return true;
+            }
+        }
+        //don't forget to return false if the loop finds no value
+        return false;
+    }
+//Similarly Create a method to remove a car from the array
+    public boolean removeCar(int carRegIn)
+        {
+        for(int i=0; i< cars.length; i+=1)
+            {   if(cars[i] != null) //Is there anyone in this element?
+                {
+                    //Is this the car we are looking for?
+                    if(cars[i].getRegistration().equals(carRegIn))
+                    {
+                    cars[i] = null;//Set the seat to empty
+                    return true;
+                    }
+                }
+            }  
+            return false;
+        }  
 }
