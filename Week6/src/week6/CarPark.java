0f92e1d2bd5e0473fd338e1bd9e2b8d05a071a83
Richard Haines <4hainr84@solent.ac.uk>
2019-10-29 16:23:08 +0000

    Added methods countCars & countSpaces to CarPark class

--- Week6/src/week6/CarPark.java
+++ Week6/src/week6/CarPark.java
@@ -2,13 +2,16 @@
 package week6;
 
 public class CarPark {
+    
 //Start with an array attribute to hold Cars
     private Car[] cars;
+    
 //Create a constructor which instantiates he array with a limit of 15 cars
     public CarPark()
     {
         cars = new Car[15];
     }
+    
 //Create a method to allow cars to be added to the CarPark object
     public boolean addCar(Car carIn)
     {
@@ -26,6 +29,7 @@ public class CarPark {
         //don't forget to return false if the loop finds no value
         return false;
     }
+    
 //Similarly Create a method to remove a car from the array
     public boolean removeCar(int carRegIn)
         {
@@ -42,4 +46,39 @@ public class CarPark {
             }  
             return false;
         }  
+    
+//Create a method to count the Cars and Spaces in the CarPark
+    public int countCars()
+    {
+        //first create a variable to hold the count as it increases...
+        int numberOfCars = 0;
+        //while we go through the loop and
+        for(int i=0; i<cars.length; i++)
+        {
+            if(cars[i] != null) //if the element has a car in it
+            {
+               numberOfCars++; //add one to the number of cars
+            }
+        }
+        return numberOfCars; //return number of cars to what called the method
+    }
+    
+//Create a method to count the Spaces in the CarPark
+    public int countSpaces()
+    {
+        /*first create a variable to hold the count as it increases
+        and initialise the count as the size of the CarPark*/
+        int numberOfSpaces = cars.length;
+        //while we go through the loop and
+        for(int i=0; i<cars.length; i++)
+        {
+            if(cars[i] != null) //if the element has a car in it
+            {
+               numberOfSpaces--; //remove one from the number of spaces
+            }
+        }
+        return numberOfSpaces; //return number of spaces to what called the method
+    }
+    
+    
 }
