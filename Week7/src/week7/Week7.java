a9fd8aad1b0417571fd12d3e57796c921a428e7d
Richard Haines <4hainr84@solent.ac.uk>
2019-11-05 16:19:54 +0000

    added output to main

--- Week7/src/week7/Week7.java
+++ Week7/src/week7/Week7.java
@@ -10,10 +10,12 @@ public class Week7 {
         Employee carol = new Admin("Carol",22000,37,25,null);
         Employee david = new Manager("David",50000,37,35,15);
         
-       alice.getPay();
-       bob.getPay();
-       carol.getPay();
-       david.getPay();
+       System.out.println(alice.displayDetails(alice.getName()));
+       System.out.println(bob.displayDetails(bob.getName()));
+       System.out.println(carol.displayDetails(carol.getName()));
+       System.out.println(david.displayDetails(david.getName()));
+        
+       
        
         
     }
