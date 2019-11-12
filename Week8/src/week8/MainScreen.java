442828b052f7791c44021892a81d1f46e7038f50
Richard Haines <4hainr84@solent.ac.uk>
2019-11-12 16:37:47 +0000

    Adjusted GUI layout to make textboxes fill the width of the column

--- Week8/src/week8/MainScreen.java
+++ Week8/src/week8/MainScreen.java
@@ -1,7 +1,18 @@
 
 package week8;
 
+/*
+For each new item we use in the Construction of the GUI we basically:
+    (i)     Import the class
+    (ii)    Add the thing as an attribute
+    (iii)   Create the item in the constructor
+    (iv)    Use the item in a Method
+
+We can add the attribute first and NetBeans will prompt us to import the class
+*/
+
 import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -16,21 +27,26 @@ public class MainScreen extends JFrame implements ActionListner
     private JLabel lblMake;
     private JLabel lblModel;
     private JLabel lblStatus;
-
-    private JTextField txtRegNo;
+    private JTextField txtRegistration;
     private JTextField txtColour;
     private JTextField txtMake;
     private JTextField txtModel;
-
     private JButton btnAdd;
     private JButton btnRemove;
+    private GridBagConstraints constraints;
     
     //CONSTRUCTOR - and methods used in the constructor
     MainScreen()
     {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new GridBagLayout());
+        constraints = new GridBagConstraints();
+        //SET UP COMPONENTS
+        initComponents();
+        //Create the layout
+        layoutComponents();
     }
+    
     public void initComponents()
     {
         lblTitle = new JLabel("Car park console");
@@ -40,7 +56,7 @@ public class MainScreen extends JFrame implements ActionListner
         lblModel = new JLabel("Model");
         lblStatus = new JLabel("The car park is empty");
         
-        txtRegNo = new JTextField();
+        txtRegistration = new JTextField();
         txtColour = new JTextField();
         txtMake = new JTextField();
         txtModel = new JTextField();
@@ -49,6 +65,83 @@ public class MainScreen extends JFrame implements ActionListner
         btnRemove = new JButton("Remove car");
     }
     
+    public void layoutComponents()
+    {
+        constraints.gridx = 1;
+        constraints.gridy = 1;
+        constraints.gridwidth = 5;
+        constraints.gridheight = 1;
+        this.add(lblTitle, constraints);
+        
+        constraints.gridx = 1;
+        constraints.gridy = 3;
+        constraints.gridwidth = 2;
+        constraints.gridheight = 1;
+        this.add(lblRegistration, constraints);
+        
+        constraints.gridx = 1;
+        constraints.gridy = 5;
+        constraints.gridwidth = 2;
+        constraints.gridheight = 1;
+        this.add(lblColour, constraints);
+        
+        constraints.gridx = 1;
+        constraints.gridy = 7;
+        constraints.gridwidth = 2;
+        constraints.gridheight = 1;
+        this.add(lblMake, constraints);
+        
+        constraints.gridx = 1;
+        constraints.gridy = 9;
+        constraints.gridwidth = 2;
+        constraints.gridheight = 1;
+        this.add(lblModel, constraints);
+        
+        constraints.gridx = 4;
+        constraints.gridy = 3;
+        constraints.gridwidth = 2;
+        constraints.gridheight = 1;
+        constraints.fill = GridBagConstraints.HORIZONTAL; //Make the text box fill the width of the column
+        this.add(txtRegistration, constraints);
+        
+        constraints.gridx = 4;
+        constraints.gridy = 5;
+        constraints.gridwidth = 2;
+        constraints.gridheight = 1;
+        this.add(txtColour, constraints);
+        
+        constraints.gridx = 4;
+        constraints.gridy = 7;
+        constraints.gridwidth = 2;
+        constraints.gridheight = 1;
+        this.add(txtMake, constraints);
+        
+        constraints.gridx = 4;
+        constraints.gridy = 9;
+        constraints.gridwidth = 2;
+        constraints.gridheight = 1;
+        this.add(txtModel, constraints);
+        
+        constraints.fill = GridBagConstraints.NONE; //Remove the 'fill the width of the column' constraint
+        constraints.gridx = 1;
+        constraints.gridy = 11;
+        constraints.gridwidth = 5;
+        constraints.gridheight = 1;
+        this.add(lblStatus, constraints);
+        
+        constraints.gridx = 1;
+        constraints.gridy = 13;
+        constraints.gridwidth = 2;
+        constraints.gridheight = 1;
+        this.add(btnAdd, constraints);
+        
+        constraints.gridx = 4;
+        constraints.gridy = 13;
+        constraints.gridwidth = 2;
+        constraints.gridheight = 1;
+        this.add(btnRemove, constraints);
+        
+    }
     
     
 }
--- Week8/src/week8/Week8.java
+++ Week8/src/week8/Week8.java
@@ -1,21 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+
 package week8;
 
-/**
- *
- * @author 4hainr84
- */
+
+
 public class Week8 {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        MainScreen gui = new MainScreen();
+        gui.pack();
+        gui.setVisible(true);
     }
     
 }
