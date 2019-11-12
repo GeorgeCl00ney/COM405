8f61a26d28e081bc24f5ce4178a42204342b374f
Richard Haines <4hainr84@solent.ac.uk>
2019-11-12 15:57:13 +0000

    Attributes for the Main Screen class added and a basic constructor without and ACTINO functionality

--- Week8/src/week8/MainScreen.java
+++ Week8/src/week8/MainScreen.java
@@ -1,14 +1,54 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package week8;
 
-/**
- *
- * @author 4hainr84
- */
-public class MainScreen {
+import java.awt.GridBagLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class MainScreen extends JFrame implements ActionListner
+{
+    //ATTRIBUTES
+    private JLabel lblTitle;
+    private JLabel lblRegistration;
+    private JLabel lblColour;
+    private JLabel lblMake;
+    private JLabel lblModel;
+    private JLabel lblStatus;
+
+    private JTextField txtRegNo;
+    private JTextField txtColour;
+    private JTextField txtMake;
+    private JTextField txtModel;
+
+    private JButton btnAdd;
+    private JButton btnRemove;
+    
+    //CONSTRUCTOR - and methods used in the constructor
+    MainScreen()
+    {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new GridBagLayout());
+    }
+    public void initComponents()
+    {
+        lblTitle = new JLabel("Car park console");
+        lblRegistration = new JLabel("Reg no.");
+        lblColour = new JLabel("Colour");
+        lblMake = new JLabel("Make");
+        lblModel = new JLabel("Model");
+        lblStatus = new JLabel("The car park is empty");
+        
+        txtRegNo = new JTextField();
+        txtColour = new JTextField();
+        txtMake = new JTextField();
+        txtModel = new JTextField();
+        
+        btnAdd = new JButton("Add car");
+        btnRemove = new JButton("Remove car");
+    }
+    
+    
     
 }
