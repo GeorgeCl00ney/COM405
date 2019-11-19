6c6491fa9adae5e74b2ce67b2beb32304453a60e
Richard Haines <4hainr84@solent.ac.uk>
2019-11-19 15:44:14 +0000

    Removed all extraneous comments

--- Week3/src/week9_car/Car.java
+++ Week3/src/week9_car/Car.java
@@ -1,15 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+
 package week9_car;
 
-/*
-Task 1 – Creating a Car Class
-First, we will create a class.
-We will later use this to create car objects.
- */
 public class Car {
     //Attributes
     private String make;
@@ -19,12 +11,7 @@ public class Car {
     private int currentSpeed;
     private int maxSpeed;
     private int fuelLevel;
-/*
-Task 1.2 - Getter Methods
-For every attribute in your class, create a getter method
-Task 1.3 - Setter Methods
-For every attribute in your class, create a setter method
-*/
+
 //getter methods
     public String getMake()
     {
@@ -83,32 +70,11 @@ For every attribute in your class, create a setter method
     {
         fuelLevel = fuelLevelIn;
     }
-    /*
-    Task 1.4 - Methods
-    Create the following methods
-    1. accelerate – increases the current speed of the car,
-    decreases the fuel level
-    2. brake – decreases the current speed of the car
-    3. refuel – increases the fuel level of the car
-    */
+    
     public void accelerate(int speedIncrease)
     {
-        // 1. accelerate – increases the current speed of the car
-        // , decreases the fuel level
-        //currentSpeed +=1;
-        //fuelLevel -=1;
-        /*
-        **FURTHER TASKS 1
-        1. Modify the accelerate method so that it takes an integer
-        as a parameter, this will determine how much the current
-        speed is increased by. Alter the main method appropriately.
-        */
         currentSpeed = currentSpeed + speedIncrease;
         fuelLevel = fuelLevel - speedIncrease;
-        /*
-        3. The current speed should not exceed the maximum speed of 
-        the car. Modify the accelerate method to ensure this does not
-        happen. Test in the main method.*/
         if(fuelLevel == 0)
                 {
                   currentSpeed =  0; 
@@ -117,28 +83,10 @@ For every attribute in your class, create a setter method
                 {
                   currentSpeed =  maxSpeed; 
                 }
-        /*
-        5. If the fuel level is zero, the car should not be able to
-        accelerate. Modify the accelerate method so that the current
-        speed is, instead, reset to zero should the car have no fuel.
-        Test in the main method.
-        */
-        
     }
     public void brake(int speedDecrease)
     {
-    //currentSpeed -=1;
-        /*
-        **FURTHER TASKS 1
-        2. Add the same modification to the brake method.
-        Alter the main method appropriately.
-        */
         currentSpeed = currentSpeed - speedDecrease;
-        /*
-        4. The current speed cannot fall below zero, modify
-        the brake method to ensure this does not happen.
-        Test in the main method.
-        */
         if(currentSpeed < 0)
                 {
                   currentSpeed = 0; 
@@ -152,14 +100,7 @@ For every attribute in your class, create a setter method
             fuelLevel = 100;
         }
     }
-/*
-***Task 2 – Constructors***
-Now we will be adding constructors to our class.
-Task 2.1 – Constructor without Parameters
-Add a constructor that takes no parameters to your class.
-Task 2.2 – Constructor with parameters
-Add a constructor that takes parameters to your class.
-*/
+
     public Car()
         {
             make = "Wacky Racer";
--- Week3/src/week9_car/Week9_car.java
+++ Week3/src/week9_car/Week9_car.java
@@ -1,94 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package week9_car;
 
-/**
- *
- * @author 4hainr84
- */
 public class Week9_car {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-       /*Task 1.5 – Main Method
-        Create a main method in which you will:
-        1. Create at least 2 car objects
-        2. Assign them values using the setter methods
-        3. Output the value of their attributes to the screen
-        4. Manipulate the values using the various methods you created earlier
-        5. Output the values again to ensure they have changed.
-         */
-       
-       /*
-       Car c1 = new Car();
-       Car c2 = new Car();
-       c1.setMake("Ford");
-       c1.setModel("Prefect");
-       c1.setYear(1975);
-       c1.setIsMotValid(false);
-       c1.setCurrentSpeed(42);
-       c1.setMaxSpeed(88);
-       c1.setFuelLevel(24);
-       c2.setMake("DMC");
-       c2.setModel("Delorean");
-       c2.setYear(1956);
-       c2.setIsMotValid(false);
-       c2.setCurrentSpeed(87);
-       c2.setMaxSpeed(120);
-       c2.setFuelLevel(80);
-       System.out.println("The attributes of Car 1 are currently:");
-       System.out.println("Make: "+c1.getMake());
-       System.out.println("Model: "+c1.getModel());
-       System.out.println("Year of manufacture: "+c1.getYear());
-       System.out.println("Is MOT valid?: "+c1.getIsMotValid());
-       System.out.println("Current speed: "+c1.getCurrentSpeed());
-       System.out.println("Max Speed: "+c1.getMaxSpeed());
-       System.out.println("Fuel level: "+c1.getFuelLevel());
-       System.out.println("The attributes of Car 2 are currently:");
-       System.out.println("Make: "+c2.getMake());
-       System.out.println("Model: "+c2.getModel());
-       System.out.println("Year of manufacture: "+c2.getYear());
-       System.out.println("Is MOT valid?: "+c2.getIsMotValid());
-       System.out.println("Current speed: "+c2.getCurrentSpeed());
-       System.out.println("Max Speed: "+c2.getMaxSpeed());
-       System.out.println("Fuel level: "+c2.getFuelLevel());
-       c1.accelerate(10);
-       c2.brake(2);
-       c2.brake(3);
-       c2.brake(5);
-       c2.refuel();
-       System.out.println("The attributes of Car 1 are currently:");
-       System.out.println("Make: "+c1.getMake());
-       System.out.println("Model: "+c1.getModel());
-       System.out.println("Year of manufacture: "+c1.getYear());
-       System.out.println("Is MOT valid?: "+c1.getIsMotValid());
-       System.out.println("Current speed: "+c1.getCurrentSpeed());
-       System.out.println("Max Speed: "+c1.getMaxSpeed());
-       System.out.println("Fuel level: "+c1.getFuelLevel());
-       System.out.println("The attributes of Car 2 are currently:");
-       System.out.println("Make: "+c2.getMake());
-       System.out.println("Model: "+c2.getModel());
-       System.out.println("Year of manufacture: "+c2.getYear());
-       System.out.println("Is MOT valid?: "+c2.getIsMotValid());
-       System.out.println("Current speed: "+c2.getCurrentSpeed());
-       System.out.println("Max Speed: "+c2.getMaxSpeed());
-       System.out.println("Fuel level: "+c2.getFuelLevel());
-       */
-       
-       
-       /*
-       ***Task 2 – Constructors***
-       Task 2.3 – Main Method
-       Modify your main method so that it makes use of the
-       new constructors. Make sure both are tested and you
-       print the values of each attribute to the screen to
-       ensure they work.
-       */
        Car c3 = new Car();
        Car c4 = new Car("Creepy Coup",999);
        System.out.println("The attributes of Car 3 are currently:");
@@ -107,10 +22,7 @@ public class Week9_car {
        System.out.println("Current speed: "+c4.getCurrentSpeed());
        System.out.println("Max Speed: "+c4.getMaxSpeed());
        System.out.println("Fuel level: "+c4.getFuelLevel());
-//       c3.refuel();
-//       c4.refuel();
-//       c3.accelerate(5);
-//       c3.accelerate(10);
+       
        c3.accelerate(90);
        System.out.println("The attributes of Car 3 are currently:");
        System.out.println("Make: "+c3.getMake());
