9681c7fc5eac96cfd9d2bcf4733f71611a27e968
Richard Haines <4hainr84@solent.ac.uk>
2019-11-26 15:47:16 +0000

    Added attributes,constructor and override method for Lorry class

--- Week10/Week10/src/week10/Lorry.java
+++ Week10/Week10/src/week10/Lorry.java
@@ -3,10 +3,26 @@ package week10;
 
 public class Lorry extends Vehicle
 {
+    //ATTRIBUTES
+    double chargeIncrement = 5.00;
+    double chargeFloor = 10.00;
+    double weightFloor = 8000;
+
+    //CONSTRUCTOR
+    public Lorry(String regIn, double weightIn) 
+    {
+        super(regIn, weightIn);
+    }
+    
     //Override the Abstract method from the parent class
     @Override
     public double CalculateFee() 
     {
-        
+        if(weight < weightFloor)
+        {
+            return chargeFloor;
+        }
+        return (chargeIncrement+chargeFloor);
     }
+
 }
